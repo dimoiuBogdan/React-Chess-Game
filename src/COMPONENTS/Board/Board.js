8fe500baaa298286338f9aad7356d15490ca1575
Bogdyapple14 <bogdybogdan34@gmail.com>
2021-07-13 00:25:54 +0300

    Looks more like react now

--- src/COMPONENTS/Board/Board.js
+++ src/COMPONENTS/Board/Board.js
@@ -1,88 +1,91 @@
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Tile from "../Tile/Tile";
 import "./Board.scss";
 
-let initialBoard = [];
-
-const addPiece = (x, y, image) => {
-  initialBoard.push({
-    image,
-    x,
-    y,
-  });
-};
-
-const populatePieces = () => {
-  // Adaugam pionii de sus
-  for (let i = 0; i < 8; i++) addPiece(i, 6, "PIECES_IMAGES/pawn_b.png");
-
-  // Adaugam pionii de jos
-  for (let j = 0; j < 8; j++) addPiece(j, 1, "PIECES_IMAGES/pawn_w.png");
-
-  const backLinePiecesPositions = [
-    // Turele
-    {
-      x: 0,
-      image: "PIECES_IMAGES/rook_b.png",
-    },
-    {
-      x: 7,
-      image: "PIECES_IMAGES/rook_b.png",
-    },
-    // Caii
-    {
-      x: 1,
-      image: "PIECES_IMAGES/knight_b.png",
-    },
-    {
-      x: 6,
-      image: "PIECES_IMAGES/knight_b.png",
-    },
-    // Nebunii
-    {
-      x: 2,
-      image: `PIECES_IMAGES/bishop_b.png`,
-    },
-    {
-      x: 5,
-      image: "PIECES_IMAGES/bishop_b.png",
-    },
-    // Regii
-    {
-      x: 3,
-      y: 7,
-      image: "PIECES_IMAGES/king_b.png",
-    },
-    // Reginele
-    {
-      x: 4,
-      image: "PIECES_IMAGES/queen_b.png",
-    },
-  ];
-
-  backLinePiecesPositions.forEach((piece) => {
-    // Pentru a injumatati piesele, facem un for care verifica daca pozitia este 0 ( caz in care le plasam pe cele de jos ) sau 1 ( caz in care le plasam
-    // pe cele de sus, respectiv de pe randul 7 )
-    for (let pos = 0; pos < 2; pos++)
-      addPiece(
-        piece.x,
-        pos === 0 ? 0 : 7,
-        // daca pozitia este 0 ( suntem pe y 0 ) schimbam culoarea pieselor in alb
-        pos === 0 ? piece.image.replace("_b", "_w") : piece.image
-      );
-  });
-};
-populatePieces();
-
 export const Board = () => {
+  let initialBoard = [];
   let board = [];
   const yAxis = [1, 2, 3, 4, 5, 6, 7, 8];
   const xAxis = ["a", "b", "c", "d", "e", "f", "g", "h"];
-  const [pieces, setPieces] = useState(initialBoard);
+  const [pieces, setPieces] = useState([]);
   const [gridX, setGridX] = useState(0);
   const [gridY, setGridY] = useState(0);
   const [grabbedPiece, setGrabbedPiece] = useState(null);
   const boardRef = useRef(null);
+
+  const addPiece = (x, y, image) => {
+    initialBoard.push({
+      image,
+      x,
+      y,
+    });
+  };
+
+  const populatePieces = () => {
+    // Adaugam pionii de sus
+    for (let i = 0; i < 8; i++) addPiece(i, 6, "PIECES_IMAGES/pawn_b.png");
+
+    // Adaugam pionii de jos
+    for (let j = 0; j < 8; j++) addPiece(j, 1, "PIECES_IMAGES/pawn_w.png");
+
+    const backLinePiecesPositions = [
+      // Turele
+      {
+        x: 0,
+        image: "PIECES_IMAGES/rook_b.png",
+      },
+      {
+        x: 7,
+        image: "PIECES_IMAGES/rook_b.png",
+      },
+      // Caii
+      {
+        x: 1,
+        image: "PIECES_IMAGES/knight_b.png",
+      },
+      {
+        x: 6,
+        image: "PIECES_IMAGES/knight_b.png",
+      },
+      // Nebunii
+      {
+        x: 2,
+        image: `PIECES_IMAGES/bishop_b.png`,
+      },
+      {
+        x: 5,
+        image: "PIECES_IMAGES/bishop_b.png",
+      },
+      // Regii
+      {
+        x: 3,
+        y: 7,
+        image: "PIECES_IMAGES/king_b.png",
+      },
+      // Reginele
+      {
+        x: 4,
+        image: "PIECES_IMAGES/queen_b.png",
+      },
+    ];
+
+    backLinePiecesPositions.forEach((piece) => {
+      // Pentru a injumatati piesele, facem un for care verifica daca pozitia este 0 ( caz in care le plasam pe cele de jos ) sau 1 ( caz in care le plasam
+      // pe cele de sus, respectiv de pe randul 7 )
+      for (let pos = 0; pos < 2; pos++)
+        addPiece(
+          piece.x,
+          pos === 0 ? 0 : 7,
+          // daca pozitia este 0 ( suntem pe y 0 ) schimbam culoarea pieselor in alb
+          pos === 0 ? piece.image.replace("_b", "_w") : piece.image
+        );
+    });
+  };
+
+  useEffect(() => {
+    populatePieces();
+    setPieces(initialBoard);
+  }, []);
 
   const returnGridValues = (e) => {
     const buggedXValue = Math.floor(
@@ -99,7 +102,7 @@ export const Board = () => {
     for (let j = yAxis.length - 1; j >= 0; j--)
       for (let i = 0; i < xAxis.length; i++) {
         let image = null;
-        initialBoard.forEach((piece) => {
+        pieces.forEach((piece) => {
           // Verificam daca pozitiile la care am ajuns ([i][j]) sunt egale cu pozitiile unei piese, caz in care plasam imaginea piesei respective pe tabla
           if (piece.x === i && piece.y === j) image = piece.image;
         });
