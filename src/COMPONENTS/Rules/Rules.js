2c1878b0fbffd7d2e762881fd31dde83b2f779c9
Bogdyapple14 <bogdybogdan34@gmail.com>
2021-07-15 12:08:48 +0300

    Commented rules.js

--- src/COMPONENTS/Rules/Rules.js
+++ src/COMPONENTS/Rules/Rules.js
@@ -15,30 +15,37 @@ export const isValidMove = (
 };
 
 const pawnRules = (prevX, prevY, newX, newY, pieceType, team, board) => {
+  // Cazurile in care pionul poate avansa 2 patratele
   const firstPawnRow = team === "our" ? 1 : 6;
+
   const pawnDirection = team === "our" ? 1 : -1;
 
   if (tileIsEmpty(newX, newY, board) && prevX === newX) {
     // Programarea miscarilor
     if (prevY !== firstPawnRow) {
+      // Daca nu este pe rand special, poate avansa doar 1
       if (newY - prevY === pawnDirection) return true;
-    } else if (newY - prevY === 2 * pawnDirection) {
-      if (tileIsEmpty(newX, newY - pawnDirection, board)) return true;
-    } else if (newY - prevY === pawnDirection) return true;
+      // Pe rand special, poate avansa 2
+    } else if (
+      newY - prevY === 2 * pawnDirection &&
+      tileIsEmpty(newX, newY - pawnDirection, board)
+    )
+      return true;
+    else if (newY - prevY === pawnDirection) return true;
   } /*Programarea atacului*/ else if (
     newX - prevX === -1 &&
     newY - prevY === pawnDirection
-  ) {
+  )
     // Sus sau jos, stanga
     return opponentOnTile(newX, newY, board, team);
-  } else if (newX - prevX === 1 && newY - prevY === pawnDirection) {
+  else if (newX - prevX === 1 && newY - prevY === pawnDirection)
     // Sus sau jos, dreapta
     return opponentOnTile(newX, newY, board, team);
-  }
   return false;
 };
 
 const tileIsEmpty = (tileX, tileY, board) => {
+  // Verificam daca exista o piesa cu aceleasi coordonate ca cele ale patratului(tileX,tileY)
   const pieceOnTile = board.find(
     (piece) => piece.x === tileX && piece.y === tileY
   );
@@ -46,6 +53,7 @@ const tileIsEmpty = (tileX, tileY, board) => {
 };
 
 const opponentOnTile = (tileX, tileY, board, team) => {
+  // Verificam daca exista deja o piesa pe patratul unde vrem sa mergem si daca este in echipa opusa
   const enemy = board.find(
     (piece) => piece.x === tileX && piece.y === tileY && piece.team !== team
   );
